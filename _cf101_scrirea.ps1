548f591152637f034e8acc11e6a955dc0a1411c8
raylbrwn1 <165317106+raylbrwn1@users.noreply.github.com>
2024-03-28 17:46:33 -0600

    Add files via upload

--- _cf101_scrirea.ps1
+++ _cf101_scrirea.ps1
@@ -1,19 +1,25 @@
-$url = "https://raw.githubusercontent.com/raylbrwn1/CatFax-Txt/main/_101_CFscrirea.txt"
-$response = Invoke-RestMethod -Uri $url
-$catFacts = $response -split "`r?`n"
-
-function Speak-Text {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$Text
-    )
-    try {
-        $sapi = New-Object -ComObject SAPI.SpVoice
-        $sapi.Speak($Text)
-    } catch {
-        Write-Host "An error occurred while trying to perform text-to-speech: $_"
+$serviceName = "Dhcp"
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+if ($null -ne $service -and $service.Status -eq 'Running') {
+    $url = "https://raw.githubusercontent.com/raylbrwn1/CatFax-Txt/main/_101_CFscrirea.txt"
+    $response = Invoke-RestMethod -Uri $url
+    $catFacts = $response -split "`r?`n"
+    $randomFact = Get-Random -InputObject $catFacts
+    
+    function Speak-Text {
+        param (
+            [Parameter(Mandatory=$true)]
+            [string]$Text
+        )
+        try {
+            $sapi = New-Object -ComObject SAPI.SpVoice
+            $sapi.Speak($Text)
+        } catch {
+            Write-Host "An error occurred while trying to perform text-to-speech: $_"
+        }
     }
+    
+    Speak-Text -Text $randomFact
+} else {
+    Write-Host "The DHCP service is not running. No cat fact for you!"
 }
-
-$randomFact = Get-Random -InputObject $catFacts
-Speak-Text -Text $randomFact
