12a35e115eb311affb6ca0c4278a17fa5c965c63
raylbrwn1 <165317106+raylbrwn1@users.noreply.github.com>
2024-03-28 18:00:13 -0600

    Update _cf101_scrirea.ps1

--- _cf101_scrirea.ps1
+++ _cf101_scrirea.ps1
@@ -1,20 +1,13 @@
-# Define the URL for fetching cat facts
 $url = "https://raw.githubusercontent.com/raylbrwn1/CatFax-Txt/main/_101_CFscrirea.txt"
-
-# Fetch the cat facts
 $response = Invoke-RestMethod -Uri $url
-
-# Split the response into individual cat facts
 $catFacts = $response -split "`r?`n"
 
-# Function to perform text-to-speech on a given text
 function Speak-Text {
     param (
         [Parameter(Mandatory=$true)]
         [string]$Text
     )
     try {
-        # Create a new SAPI.SpVoice object and use it to speak the text
         $sapi = New-Object -ComObject SAPI.SpVoice
         $sapi.Speak($Text)
     } catch {
@@ -22,27 +15,6 @@ function Speak-Text {
     }
 }
 
-# Outer loop to iterate 4 times
-1..4 | ForEach-Object {
-    Write-Output "Iteration $_ of the outer loop started."
+$randomFact = Get-Random -InputObject $catFacts
 
-    # Inner loop to read cat facts 5 times
-    1..5 | ForEach-Object {
-        # Select a random cat fact
-        $randomFact = Get-Random -InputObject $catFacts
-        
-        # Use the Speak-Text function to read the cat fact
-        Speak-Text -Text $randomFact
-        
-        # Wait a bit before reading the next fact (optional, adjust as desired)
-        Start-Sleep -Seconds 10
-    }
-
-    # Check if it's not the last iteration before the break
-    if ($_ -lt 4) {
-        Write-Output "Starting a 90-minute break."
-        Start-Sleep -Seconds (90 * 60) # 90 minutes break
-    }
-}
-
-Write-Output "Process completed."
+Speak-Text -Text $randomFact
